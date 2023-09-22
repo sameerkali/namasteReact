9fca817e7e1de06f5291b88647f3ef93deac3671
Sameer Faridi <s.faridi007@gmail.com>
2023-09-22 16:49:25 +0530

    start making gaming website from scratch

--- scratch/App.js
+++ scratch/App.js
@@ -1,20 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-console.log("first")
-const heading = React.createElement(
-  "h1",
-  { id: "sameer", key: "1" },
-  "namaste everyone"
-);
-const heading2 = React.createElement(
-  "h1",
-  { id: "sameer", key: "2" },
-  "assalaammualakum everyone"
-);
-const container = React.createElement("div", { id: "container" }, [
-  heading,
-  heading2
-]);
+// const heading = React.createElement(
+//   "h1",
+//   { id: "sameer", key: "1" },
+//   "namaste everyone"
+// );
+// const heading2 = React.createElement(
+//   "h1",
+//   { id: "sameer", key: "2" },
+//   "assalaammualakum everyone"
+// );
+// console.log(heading2)
+
+// const container = React.createElement("div", { id: "container" }, [
+//   heading,
+//   heading2
+// ]);
+
+const Dam = () => {
+  return (
+    <div id="container">
+      <button>hello sameer i am a button</button>
+    </div>
+  );
+}
+const Sam = () => {
+  return (
+    <div id="container">
+      <h1 id="sameer" key="1">
+        namaste everyone
+      </h1>
+      <h1 id="sameer" key="2">
+        assalaammualakum everyone
+      </h1>
+      <Dam />
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<Sam />);
