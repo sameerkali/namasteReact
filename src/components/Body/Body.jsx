60e5102eb38ac0fce8fa61d93e544f7277ce2a6e
Sameer Faridi <s.faridi007@gmail.com>
2023-11-10 16:12:24 +0530

    remove search for now

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,3 +1,72 @@
+// import React, { useEffect, useState } from "react";
+// import "./styles.css";
+// import Card from "../Others/Card";
+// import Shimmer from "../Others/shimmar";
+// import { Link } from "react-router-dom";
+
+// const Api_url = process.env.Game_api_key;
+
+// const Body = () => {
+//   const [games, setGames] = useState([]);
+//   const [search, setSearch] = useState([]);
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     api();
+//   }, []);
+
+//   const api = async () => {
+//     const response = await fetch(Api_url);
+//     const data = await response.json();
+//     setGames(data.results);
+//     setSearch(data.results);
+//     setLoading(false);
+//   };
+
+//   const searchGame = (e) => {
+//     const searchValue = e.target.value;
+
+//     if (searchValue === "") {
+//       setSearch(null);
+//       setLoading(true);
+//     }
+//     const searchFilter = games?.filter((s) =>
+//       s.name.toLowerCase().includes(searchValue.toLowerCase())
+//     );
+//     setSearch(searchFilter);
+//   };
+
+//   if (loading) {
+//     return <Shimmer />;
+//   }
+
+//   return (
+//     <>
+//       {/* search bar */}
+//       <div className="search-container">
+//         <input
+//           type="text"
+//           className="search-input"
+//           placeholder="Search a game..."
+//           aria-label="Search"
+//           onChange={searchGame}
+//         />
+//       </div>
+
+//       {/* Body */}
+//       <div className="container-body">
+//         {search.map(({ id, name, background_image }) => (
+//           <Link key={id} to={`/game/${id}`}>
+//             <Card name={name} image={background_image} />
+//           </Link>
+//         ))}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Body;
+
 import React, { useEffect, useState } from "react";
 import "./styles.css";
 import Card from "../Others/Card";
@@ -8,7 +77,6 @@ const Api_url = process.env.Game_api_key;
 
 const Body = () => {
   const [games, setGames] = useState([]);
-  const [search, setSearch] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -18,22 +86,8 @@ const Body = () => {
   const api = async () => {
     const response = await fetch(Api_url);
     const data = await response.json();
-    setGames(data.results);
-    setSearch(data.results);
+    setGames(data.results.slice(0, 12));
     setLoading(false);
-  };
-
-  const searchGame = (e) => {
-    const searchValue = e.target.value;
-
-    if (searchValue === "") {
-      setSearch(null);
-      setLoading(true);
-    }
-    const searchFilter = games?.filter((s) =>
-      s.name.toLowerCase().includes(searchValue.toLowerCase())
-    );
-    setSearch(searchFilter);
   };
 
   if (loading) {
@@ -42,20 +96,8 @@ const Body = () => {
 
   return (
     <>
-      {/* search bar */}
-      <div className="search-container">
-        <input
-          type="text"
-          className="search-input"
-          placeholder="Search a game..."
-          aria-label="Search"
-          onChange={searchGame}
-        />
-      </div>
-
-      {/* Body */}
       <div className="container-body">
-        {search.map(({ id, name, background_image }) => (
+        {games.map(({ id, name, background_image }) => (
           <Link key={id} to={`/game/${id}`}>
             <Card name={name} image={background_image} />
           </Link>
