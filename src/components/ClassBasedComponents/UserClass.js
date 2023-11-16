e8133e01e5f9bbbb4cd0c18290034877ad9f71ab
Sameer Faridi <s.faridi007@gmail.com>
2023-11-16 13:43:27 +0530

    thats how we can make an API call in class comp

--- src/components/ClassBasedComponents/UserClass.js
+++ src/components/ClassBasedComponents/UserClass.js
@@ -2,21 +2,32 @@ import React from "react";
 
 class UserClass extends React.Component {
   constructor(props) {
+    console.log("constructor child");
     super(props); // HW why do we write super props
-    console.log(props.name);
     this.state = {
       count: 0,
       coun1: 1,
       coun2: 2
     };
   }
+  async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/sameerkali");
+    const response = await data.json();
+    console.log(response)
+    console.log("componentDidMount child");
+  }
   render() {
+    console.log("render child");
     return (
       <>
         <h1>{this.state.count}</h1>
+        <h3>{this.state.coun1}</h3>
         <button
           onClick={() => {
-            this.setState({ count: this.state.count + 1 });
+            this.setState({
+              count: this.state.count + 1,
+              coun1: this.state.coun1 + 10 // fixed the typo here
+            });
           }}
         >
           Increase the count
--- src/components/Others/about.jsx
+++ src/components/Others/about.jsx
@@ -1,39 +1,84 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import User from "../ClassBasedComponents/User";
 import UserClass from "../ClassBasedComponents/UserClass";
 
-const Profile = () => {
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        marginTop: "20rem",
-        alignItems: "center",
-        justifyContent: "center",
-        width: "100%",
-        height: "100%",
-        overflow: "hidden"
-      }}
-    >
-      <UserClass name="sameer faridi from props 222222" />
-
-      <h1 style={{ fontSize: "3rem" }}>Sameer's Profile</h1>
-      <Link
-        to="/"
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor Parent")
+  }
+  componentDidMount() {
+    console.log("componentDidMount Parent")
+  }
+  render() {
+    console.log("render Parent")
+    return (
+      <div
         style={{
-          fontSize: "1rem",
-          color: "blue",
-          backgroundColor: "lightgray",
-          padding: "10px",
-          borderRadius: "5px"
+          display: "flex",
+          flexDirection: "column",
+          marginTop: "20rem",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+          height: "100%",
+          overflow: "hidden"
         }}
       >
-        Home
-      </Link>
-    </div>
-  );
-};
+        <UserClass name="sameer faridi from props 222222" />
+        {/* <UserClass name="sameer faridi from props 333333" /> */}
+
+        <h1 style={{ fontSize: "3rem" }}>Sameer's Profile</h1>
+        <Link
+          to="/"
+          style={{
+            fontSize: "1rem",
+            color: "blue",
+            backgroundColor: "lightgray",
+            padding: "10px",
+            borderRadius: "5px"
+          }}
+        >
+          Home
+        </Link>
+      </div>
+    );
+  }
+}
 
 export default Profile;
+
+// const Profile = () => {
+//   return (
+//     <div
+//       style={{
+//         display: "flex",
+//         flexDirection: "column",
+//         marginTop: "20rem",
+//         alignItems: "center",
+//         justifyContent: "center",
+//         width: "100%",
+//         height: "100%",
+//         overflow: "hidden"
+//       }}
+//     >
+//       <UserClass name="sameer faridi from props 222222" />
+
+//       <h1 style={{ fontSize: "3rem" }}>Sameer's Profile</h1>
+//       <Link
+//         to="/"
+//         style={{
+//           fontSize: "1rem",
+//           color: "blue",
+//           backgroundColor: "lightgray",
+//           padding: "10px",
+//           borderRadius: "5px"
+//         }}
+//       >
+//         Home
+//       </Link>
+//     </div>
+//   );
+// };
+
+// export default Profile;
