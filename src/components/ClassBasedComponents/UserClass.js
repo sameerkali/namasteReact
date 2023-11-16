d9a88c519af5005d8d0e90272a6ac4b679d6bf4e
Sameer Faridi <s.faridi007@gmail.com>
2023-11-16 17:28:14 +0530

    make an api call with compDidMount()

--- src/components/ClassBasedComponents/UserClass.js
+++ src/components/ClassBasedComponents/UserClass.js
@@ -5,35 +5,32 @@ class UserClass extends React.Component {
     console.log("constructor child");
     super(props); // HW why do we write super props
     this.state = {
-      count: 0,
-      coun1: 1,
-      coun2: 2
+      userInfo: {
+        name: "sameer",
+        location: "GhaziabaD",
+        avtar_url: ""
+      }
     };
   }
   async componentDidMount() {
     const data = await fetch("https://api.github.com/users/sameerkali");
     const response = await data.json();
-    console.log(response)
+    4;
+    this.setState({
+      userInfo: response
+    });
+    console.log(response);
     console.log("componentDidMount child");
   }
   render() {
+    const { name, location, avatar_url } = this.state.userInfo;
     console.log("render child");
+    // debugger;
     return (
       <>
-        <h1>{this.state.count}</h1>
-        <h3>{this.state.coun1}</h3>
-        <button
-          onClick={() => {
-            this.setState({
-              count: this.state.count + 1,
-              coun1: this.state.coun1 + 10 // fixed the typo here
-            });
-          }}
-        >
-          Increase the count
-        </button>
-        <p>Name: {this.props.name}</p>
-        <p>Location: Ghaziabad {this.state.coun2}</p>
+        <img src={avatar_url} />
+        <p>Name: {name}</p>
+        <p>Location: {location}</p>
         <p>Contact: @sameer_faridi_</p>
       </>
     );
