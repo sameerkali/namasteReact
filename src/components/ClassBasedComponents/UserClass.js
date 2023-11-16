7b6e33f5c02323fcf171fb89a46d88dd53aa7d56
Sameer Faridi <s.faridi007@gmail.com>
2023-11-16 09:45:39 +0530

    update state variables

--- src/components/ClassBasedComponents/UserClass.js
+++ src/components/ClassBasedComponents/UserClass.js
@@ -2,17 +2,25 @@ import React from "react";
 
 class UserClass extends React.Component {
   constructor(props) {
-    super(props);                       // HW why do we write super props
+    super(props); // HW why do we write super props
     console.log(props.name);
     this.state = {
-        count: 0,
-        coun1: 1,
-        coun2: 2
-    }
+      count: 0,
+      coun1: 1,
+      coun2: 2
+    };
   }
   render() {
     return (
       <>
+        <h1>{this.state.count}</h1>
+        <button
+          onClick={() => {
+            this.setState({ count: this.state.count + 1 });
+          }}
+        >
+          Increase the count
+        </button>
         <p>Name: {this.props.name}</p>
         <p>Location: Ghaziabad {this.state.coun2}</p>
         <p>Contact: @sameer_faridi_</p>
--- src/components/Others/about.jsx
+++ src/components/Others/about.jsx
@@ -1,26 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import User from "../ClassBasedComponents/User";
-import UserClass from '../ClassBasedComponents/UserClass'
-
+import UserClass from "../ClassBasedComponents/UserClass";
 
 const Profile = () => {
   return (
-    <div style={{
-      display: "flex",
-      flexDirection: "column",
-      marginTop: '20rem',
-      alignItems: "center",
-      justifyContent: "center",
-      width: "100%",
-      height: "100%",
-      overflow: "hidden"
-    }}>
-    <User name="sameer faridi from props 111111"/>
-    <UserClass name="sameer faridi from props 222222"/>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        marginTop: "20rem",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        height: "100%",
+        overflow: "hidden"
+      }}
+    >
+      <UserClass name="sameer faridi from props 222222" />
 
       <h1 style={{ fontSize: "3rem" }}>Sameer's Profile</h1>
-      <Link to="/" style={{ fontSize: "1rem", color: "blue", backgroundColor: "lightgray", padding: "10px", borderRadius: "5px" }}>Home</Link>
+      <Link
+        to="/"
+        style={{
+          fontSize: "1rem",
+          color: "blue",
+          backgroundColor: "lightgray",
+          padding: "10px",
+          borderRadius: "5px"
+        }}
+      >
+        Home
+      </Link>
     </div>
   );
 };
