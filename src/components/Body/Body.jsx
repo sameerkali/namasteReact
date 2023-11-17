0492d97e246c8a3bfd716eba5377dafb365a6191
Sameer Faridi <s.faridi007@gmail.com>
2023-11-17 16:22:40 +0530

    try to impliment pagination inthis code

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -3,14 +3,24 @@ import Card from "../Others/Card";
 import { Link } from "react-router-dom";
 import useMainData from "../Others/useMainData";
 import useOnlineStatus from "../Others/useOnlineStatus";
-
-const Api_url = process.env.Game_api_key;
+import { useState } from "react";
+// const Api_url = process.env.Game_api_key;
 
 const Body = () => {
+  const [page, setPage] = useState(1);
+  const Api_url = `https://api.rawg.io/api/games?key=3e49c7b55bf4452bac85435423b53726&page=${page}`;
+  console.log(Api_url);
   const games = useMainData(Api_url);
   const isOnline = useOnlineStatus();
+  const setPageHandler = () => {
+    setPage(page+1);
+  };
+  const setPageHandlerPrev = () => {
+    if (page >= 1) setPage(page-1);
+  };
 
   if (!isOnline) return <h1>You are offline 🔴</h1>;
+
   return (
     <div className="container-body">
       {games.map(({ id, name, background_image }) => (
@@ -18,6 +28,11 @@ const Body = () => {
           <Card name={name} image={background_image} />
         </Link>
       ))}
+
+      <div className="pagination">
+        <button onClick={setPageHandlerPrev}>previous</button>
+        <button onClick={setPageHandler}>next</button>
+      </div>
     </div>
   );
 };
